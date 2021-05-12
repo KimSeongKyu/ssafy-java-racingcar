1de230220444e6206c264e5b1ecb76fc29a7133a
김성규 <kskyu610@gmail.com>
2021-05-12 21:17:22 +0900

    feat:StringUtil 클래스 문자열을 ','로 분리하는 기능 구현

--- src/main/java/StringUtil.java
+++ src/main/java/StringUtil.java
@@ -1,3 +1,9 @@
-public class StringUtil {
+public final class StringUtil {
 
+    private StringUtil() {
+    }
+
+    public static String[] split(String sentence) {
+        return sentence.split(",");
+    }
 }
