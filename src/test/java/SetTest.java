b11ee3990d4257310a1ce4ba8e47faa3505d26e7
김성규 <kskyu610@gmail.com>
2021-05-12 23:29:44 +0900

    test:Set이 입력값을 원소로 가지면 true, 갖지 않으면 false를 반환하는지 확인하는 테스트

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -39,5 +40,12 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void containOneTwoThreeTest(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @DisplayName("Set이 입력값을 원소로 가지면 true, 갖지 않으면 false를 반환하는지 확인하는 테스트")
+    @ParameterizedTest(name = "{index}. Set이 입력값 {0}에 대해 올바른 결과를 반환하는지 확인하는 테스트")
+    @CsvSource(value = {"0,false","1,true", "2,true", "3,true", "4,false"})
+    void containTest(int number, boolean expectedResult) {
+        assertThat(numbers.contains(number)).isEqualTo(expectedResult);
     }
 }
