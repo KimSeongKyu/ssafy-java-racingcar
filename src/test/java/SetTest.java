3d38401a305e532b09ede4531554a90c0a9479b4
김성규 <kskyu610@gmail.com>
2021-05-12 22:57:49 +0900

    test:Set의 크기를 확인하는 테스트 작성

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("Set의 크기를 확인하는 테스트")
+    @Test
+    void getSizeTest() {
+        // given
+        int expectedSetSize = 3;
+
+        // when
+        int setSize = numbers.size();
+
+        // then
+        assertThat(setSize).isEqualTo(expectedSetSize);
+    }
+}
