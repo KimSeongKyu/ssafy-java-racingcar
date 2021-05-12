271bded77e60a1f587e46132244fcd9f8d07e85a
김성규 <kskyu610@gmail.com>
2021-05-12 22:45:12 +0900

    feat:StringUtil 클래스 문자열 내에 있는 특정 위치의 문자를 가져오는 기능 구현

--- src/main/java/StringUtil.java
+++ src/main/java/StringUtil.java
@@ -10,4 +10,8 @@ public final class StringUtil {
     public final static String removeBracket(String sentenceWithBracket) {
         return sentenceWithBracket.substring(1, sentenceWithBracket.length()-1);
     }
+
+    public final static char getCharacterAtIndex(int index, String sentence) {
+        return sentence.charAt(index);
+    }
 }
