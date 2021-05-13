333c8ab30a46cf915b480fb13b37739678f26c39
김성규 <kskyu610@gmail.com>
2021-05-13 23:47:11 +0900

    refactor:StringUtil 클래스 리터럴을 상수로 리팩토링

--- src/main/java/StringUtil.java
+++ src/main/java/StringUtil.java
@@ -1,14 +1,17 @@
 public final class StringUtil {
 
+    private static final int ONE = 1;
+    private static final String COMMA = ",";
+
     private StringUtil() {
     }
 
     public final static String[] splitByComma(final String sentence) {
-        return sentence.split(",");
+        return sentence.split(COMMA);
     }
 
     public final static String removeBracket(final String sentenceWithBracket) {
-        return sentenceWithBracket.substring(1, sentenceWithBracket.length() - 1);
+        return sentenceWithBracket.substring(ONE, sentenceWithBracket.length() - ONE);
     }
 
     public final static char getCharacterAtIndex(final int index, final String sentence) {
