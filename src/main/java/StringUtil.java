08be5cc2a9c3afe3a88a2de789fab7b6c459f78b
김성규 <kskyu610@gmail.com>
2021-05-13 23:45:25 +0900

    refactor:StringUtil 클래스 파라미터에 final 예약어 추가 리팩토링

--- src/main/java/StringUtil.java
+++ src/main/java/StringUtil.java
@@ -3,15 +3,15 @@ public final class StringUtil {
     private StringUtil() {
     }
 
-    public final static String[] splitByComma(String sentence) {
+    public final static String[] splitByComma(final String sentence) {
         return sentence.split(",");
     }
 
-    public final static String removeBracket(String sentenceWithBracket) {
-        return sentenceWithBracket.substring(1, sentenceWithBracket.length()-1);
+    public final static String removeBracket(final String sentenceWithBracket) {
+        return sentenceWithBracket.substring(1, sentenceWithBracket.length() - 1);
     }
 
-    public final static char getCharacterAtIndex(int index, String sentence) {
+    public final static char getCharacterAtIndex(final int index, final String sentence) {
         return sentence.charAt(index);
     }
 }
