7b1e52c4aceb6e0470d5a4dfd7f6a3effd7100e0
김성규 <kskyu610@gmail.com>
2021-05-15 00:28:24 +0900

    test:addTest 메소드 숫자 타입 리팩토링

--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StringCalculatorTest {
@@ -17,16 +19,19 @@ class StringCalculatorTest {
             "-10,-20,-30",
             "10000000000,20000000000,30000000000",
             "-10000000000,20000000000,10000000000",
-            "-10000000000,-20000000000,-30000000000"
+            "-10000000000,-20000000000,-30000000000",
+            "10.7,20.8,31.5",
+            "-10.7,20.8,10.1",
+            "-10.7,-20.8,-31.5"
     })
     void addTest(String leftOperandString, String rightOperandString, String expectedSumString) {
         // given
-        long leftOperand = Long.parseLong(leftOperandString);
-        long rightOperand = Long.parseLong(rightOperandString);
-        long expectedSum = Long.parseLong(expectedSumString);
+        BigDecimal leftOperand = new BigDecimal(leftOperandString);
+        BigDecimal rightOperand = new BigDecimal(rightOperandString);
+        BigDecimal expectedSum = new BigDecimal(expectedSumString);
 
         // when
-        long sum = StringCalculator.add(leftOperand, rightOperand);
+        BigDecimal sum = StringCalculator.add(leftOperand, rightOperand);
 
         // then
         assertThat(sum).isEqualTo(expectedSum);
