0ac44d8619b31324ce977852bd991a61bccf20c1
김성규 <kskyu610@gmail.com>
2021-05-14 22:21:45 +0900

    feat:StringCalculator 클래스 두 수에 대한 덧셈 기능 구현

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,4 +2,7 @@ public final class StringCalculator {
 
     private StringCalculator(){}
 
+    public static final long add(final long leftOperand, final long rightOperand) {
+        return Math.addExact(leftOperand, rightOperand);
+    }
 }
