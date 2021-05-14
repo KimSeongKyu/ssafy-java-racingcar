8f8647ea50909cd2a7c79bd7d0a111b6c73d300d
김성규 <kskyu610@gmail.com>
2021-05-15 00:55:09 +0900

    StringCalculator 클래스 두 수에 대한 뺄셈 기능 구현

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -6,7 +6,11 @@ public final class StringCalculator {
 
     private StringCalculator(){}
 
-    public static final BigDecimal add(final BigDecimal leftOperand, final BigDecimal rightOperand) {
+    public final static BigDecimal add(final BigDecimal leftOperand, final BigDecimal rightOperand) {
         return leftOperand.add(rightOperand);
+    }
+
+    public final static BigDecimal subtract(BigDecimal leftOperand, BigDecimal rightOperand) {
+        return leftOperand.subtract(rightOperand);
     }
 }
