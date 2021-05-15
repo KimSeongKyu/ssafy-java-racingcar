50b50f070b17474d2c5ee081d7654b03e6379a29
김성규 <kskyu610@gmail.com>
2021-05-15 15:14:01 +0900

    feat:ExpressionTokenizer 클래스 입력이 null이나 빈 문자열일 때 IllegalArgument Exception을 발생시키는 기능 구현

--- src/main/java/step2/ExpressionTokenizer.java
+++ src/main/java/step2/ExpressionTokenizer.java
@@ -4,4 +4,11 @@ public final class ExpressionTokenizer {
 
     private ExpressionTokenizer() {
     }
+
+    public final static String[] tokenize(final String expression) throws IllegalArgumentException {
+        if (expression == null || expression.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        return expression.split(" ");
+    }
 }
