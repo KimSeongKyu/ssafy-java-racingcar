5153aa02b7b0d5539f57f60244a1d910c6ef36b3
김성규 <kskyu610@gmail.com>
2021-05-15 17:08:35 +0900

    feat:토큰화된 수식이 사칙연산자(+,-,*,/)만 갖고 있는지 확인하는 기능 구현

--- src/main/java/step2/ExpressionTokenizer.java
+++ src/main/java/step2/ExpressionTokenizer.java
@@ -1,6 +1,12 @@
 package step2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public final class ExpressionTokenizer {
+
+    private final static List<String> OPERATORS = new ArrayList<>(Arrays.asList("+", "-", "*", "/"));
 
     private ExpressionTokenizer() {
     }
@@ -10,5 +16,13 @@ public final class ExpressionTokenizer {
             throw new IllegalArgumentException();
         }
         return expression.split(" ");
+    }
+
+    public final static boolean hasRightOperators(final String[] tokenizedExpression) {
+        final List<String> tokenizedExpressionList = Arrays.asList(tokenizedExpression);
+
+        return tokenizedExpressionList.stream()
+                .filter(operator -> tokenizedExpressionList.indexOf(operator) % 2 == 1)
+                .anyMatch(operator -> OPERATORS.contains(operator));
     }
 }
