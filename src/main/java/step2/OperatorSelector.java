b873dff0025001a1c44d0a859e4a355e2fb70578
김성규 <kskyu610@gmail.com>
2021-05-17 00:08:53 +0900

    feat:OperatorSelector 클래스 주어진 문자열에 해당하는 Operator 클래스를 반환하는 기능 구현

---
+++ src/main/java/step2/OperatorSelector.java
@@ -0,0 +1,28 @@
+package step2;
+
+public final class OperatorSelector {
+
+    private final static String ADD = "+";
+    private final static String SUBTRACT = "-";
+    private final static String MULTIPLY = "*";
+    private final static String DIVIDE = "/";
+
+    private OperatorSelector() {
+    }
+
+    public final static Operator selectOperator(final String operatorString) {
+        if (operatorString.equals(ADD)) {
+            return new AddOperator();
+        }
+        if (operatorString.equals(SUBTRACT)) {
+            return new SubtractOperator();
+        }
+        if (operatorString.equals(MULTIPLY)) {
+            return new MultiplyOperator();
+        }
+        if (operatorString.equals(DIVIDE)) {
+            return new DivideOperator();
+        }
+        return null;
+    }
+}
