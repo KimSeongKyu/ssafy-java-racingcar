f3185f8c0a609f22a88ef6cddd677adaecb41f3f
김성규 <kskyu610@gmail.com>
2021-05-16 22:04:31 +0900

    refactor:StringCalculator 클래스 매직 넘버, 리터럴 및 메소드 정의 순서 리팩토링

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -5,7 +5,43 @@ import java.util.List;
 
 public final class StringCalculator {
 
+    private final static int FIRST_OPERAND_INDEX = 0;
+    private final static int FIRST_OPERATOR_INDEX = 1;
+    private final static int SECOND_OPERAND_INDEX = 2;
+    private final static int DIFFERENCE_BETWEEN_OPERATORS_INDEX = 2;
+    private final static int DIFFERENCE_BETWEEN_OPERANDS_INDEX = 2;
+
+    private final static String ADD = "+";
+    private final static String SUBTRACT = "-";
+    private final static String MULTIPLY = "*";
+    private final static String DIVIDE = "/";
+
     private StringCalculator() {
+    }
+
+    public final static BigDecimal calculate(final String expression) {
+        final List<String> tokenizedExpression = ExpressionTokenizer.tokenizeByEmptyString(expression);
+
+        BigDecimal result = new BigDecimal(tokenizedExpression.get(FIRST_OPERAND_INDEX));
+        for (int operatorIndex = FIRST_OPERATOR_INDEX, operandIndex = SECOND_OPERAND_INDEX;
+             operandIndex < tokenizedExpression.size();
+             operatorIndex += DIFFERENCE_BETWEEN_OPERATORS_INDEX, operandIndex += DIFFERENCE_BETWEEN_OPERANDS_INDEX) {
+
+            final String operator = tokenizedExpression.get(operatorIndex);
+            final BigDecimal operand = new BigDecimal(tokenizedExpression.get(operandIndex));
+
+            if (operator.equals(ADD)) {
+                result = add(result, operand);
+            } else if (operator.equals(SUBTRACT)) {
+                result = subtract(result, operand);
+            } else if (operator.equals(MULTIPLY)) {
+                result = multiply(result, operand);
+            } else if (operator.equals(DIVIDE)) {
+                result = divide(result, operand);
+            }
+        }
+
+        return result;
     }
 
     public final static BigDecimal add(final BigDecimal leftOperand, final BigDecimal rightOperand) {
@@ -23,33 +59,4 @@ public final class StringCalculator {
     public final static BigDecimal divide(final BigDecimal leftOperand, final BigDecimal rightOperand) {
         return leftOperand.divide(rightOperand);
     }
-
-    public final static BigDecimal calculate(final String expression) {
-        final List<String> tokenizedExpression = ExpressionTokenizer.tokenizeByEmptyString(expression);
-
-        BigDecimal result = new BigDecimal(tokenizedExpression.get(0));
-        for (int operatorIndex = 1, operandIndex = 2; operandIndex < tokenizedExpression.size();
-             operatorIndex += 2, operandIndex += 2) {
-            final String operator = tokenizedExpression.get(operatorIndex);
-            final BigDecimal operand = new BigDecimal(tokenizedExpression.get(operandIndex));
-
-            switch (operator) {
-                case "+":
-                    result = add(result, operand);
-                    break;
-                case "-":
-                    result = subtract(result, operand);
-                    break;
-                case "*":
-                    result = multiply(result, operand);
-                    break;
-                case "/":
-                    result = divide(result, operand);
-                    break;
-            }
-        }
-
-        return result;
-    }
-
 }
