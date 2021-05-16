ba7f99a6a119e241ab6aee596866ea41db5e7d86
김성규 <kskyu610@gmail.com>
2021-05-16 12:16:46 +0900

    feat:StringCalculator 클래스 주어진 수식에 대한 연산 기능 구현

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -21,4 +21,33 @@ public final class StringCalculator {
     public final static BigDecimal divide(BigDecimal leftOperand, BigDecimal rightOperand) {
         return leftOperand.divide(rightOperand);
     }
+
+    public final static BigDecimal calculate(final String expression) {
+        String[] tokenizedExpression = ExpressionTokenizer.tokenizeByEmptyString(expression);
+
+        BigDecimal result = new BigDecimal(tokenizedExpression[0]);
+        for (int operatorIndex = 1, operandIndex = 2; operandIndex < tokenizedExpression.length;
+             operatorIndex += 2, operandIndex += 2) {
+            String operator = tokenizedExpression[operatorIndex];
+            BigDecimal operand = new BigDecimal(tokenizedExpression[operandIndex]);
+
+            switch (operator) {
+                case "+":
+                    result = add(result, operand);
+                    break;
+                case "-":
+                    result = subtract(result, operand);
+                    break;
+                case "*":
+                    result = multiply(result, operand);
+                    break;
+                case "/":
+                    result = divide(result, operand);
+                    break;
+            }
+        }
+
+        return result;
+    }
+
 }
