85cc5d5d72e97deff8330c9021ddf857d152c4cc
김성규 <kskyu610@gmail.com>
2021-05-16 20:22:41 +0900

    refactor:StringUtil 클래스 의미를 보다 명확하게 리팩토링

--- src/main/java/step1/StringUtil.java
+++ src/main/java/step1/StringUtil.java
@@ -2,8 +2,9 @@ package step1;
 
 public final class StringUtil {
 
-    private static final int ONE = 1;
-    private static final String COMMA = ",";
+    private final static int INDEX_AFTER_OPEN_BRACKET = 1;
+    private final static int CLOSE_BRACKET_LENGTH = 1;
+    private final static String COMMA = ",";
 
     private StringUtil() {
     }
@@ -13,7 +14,11 @@ public final class StringUtil {
     }
 
     public final static String removeBracket(final String sentenceWithBracket) {
-        return sentenceWithBracket.substring(ONE, Math.subtractExact(sentenceWithBracket.length(), ONE));
+        return sentenceWithBracket.substring(INDEX_AFTER_OPEN_BRACKET, getIndexBeforeCloseBracket(sentenceWithBracket));
+    }
+
+    private final static int getIndexBeforeCloseBracket(final String sentenceWithBracket) {
+        return Math.subtractExact(sentenceWithBracket.length(), CLOSE_BRACKET_LENGTH);
     }
 
     public final static char getCharacterAtIndex(final int index, final String sentence) {
