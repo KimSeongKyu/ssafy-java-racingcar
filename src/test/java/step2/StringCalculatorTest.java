7fb66bad3551e988e411c1e4506a2491ed082ed4
김성규 <kskyu610@gmail.com>
2021-05-16 12:09:28 +0900

    test:주어진 수식에 대한 연산 테스트 작성

--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 package step2;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
@@ -136,5 +137,19 @@ class StringCalculatorTest {
             // when
             BigDecimal quotient = StringCalculator.divide(leftOperand, zero);
         });
+    }
+
+    @DisplayName(value = "주어진 수식에 대한 연산 테스트")
+    @Test
+    void calculateTest() {
+        // given
+        String expression = "5 + 4 - 3 * 2 / 3";
+        BigDecimal expectedResult = new BigDecimal(4);
+
+        // when
+        BigDecimal result = StringCalculator.calculate(expression);
+
+        // then
+        assertThat(result).isEqualTo(expectedResult);
     }
 }
