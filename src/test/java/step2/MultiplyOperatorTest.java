b4205bed9680e3c505ae51eda6f6ff18117e3179
김성규 <kskyu610@gmail.com>
2021-05-16 23:53:53 +0900

    test:전략 패턴을 위해 곱셈 테스트를 StringCalculatorTest에서 MultiplyOperatorTest로 리팩토링

---
+++ src/test/java/step2/MultiplyOperatorTest.java
@@ -0,0 +1,47 @@
+package step2;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MultiplyOperatorTest {
+
+    private static Operator operator;
+
+    @BeforeAll
+    static void setUp() {
+        operator = new MultiplyOperator();
+    }
+
+    @DisplayName(value = "두 수에 대한 곱셈 테스트")
+    @ParameterizedTest(name = "{index}. 피연산자: [{0},{1}] 결과: [{2}]")
+    @CsvSource(value = {
+            "0,0,0",
+            "10,20,200",
+            "-10,20,-200",
+            "-10,-20,200",
+            "10000000000,20000000000,200000000000000000000",
+            "-10000000000,20000000000,-200000000000000000000",
+            "-10000000000,-20000000000,200000000000000000000",
+            "10.7,20.8,222.56",
+            "-10.7,20.8,-222.56",
+            "-10.7,-20.8,222.56"
+    })
+    void multiplyTest(String leftOperandString, String rightOperandString, String expectedSumString) {
+        // given
+        BigDecimal leftOperand = new BigDecimal(leftOperandString);
+        BigDecimal rightOperand = new BigDecimal(rightOperandString);
+        BigDecimal expectedSum = new BigDecimal(expectedSumString);
+
+        // when
+        BigDecimal sum = operator.operate(leftOperand, rightOperand);
+
+        // then
+        assertThat(sum).isEqualTo(expectedSum);
+    }
+}
--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -13,33 +13,6 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class StringCalculatorTest {
 
-    @DisplayName(value = "두 수에 대한 곱셈 테스트")
-    @ParameterizedTest(name = "{index}. 피연산자: [{0},{1}] 결과: [{2}]")
-    @CsvSource(value = {
-            "0,0,0",
-            "10,20,200",
-            "-10,20,-200",
-            "-10,-20,200",
-            "10000000000,20000000000,200000000000000000000",
-            "-10000000000,20000000000,-200000000000000000000",
-            "-10000000000,-20000000000,200000000000000000000",
-            "10.7,20.8,222.56",
-            "-10.7,20.8,-222.56",
-            "-10.7,-20.8,222.56"
-    })
-    void multiplyTest(String leftOperandString, String rightOperandString, String expectedSumString) {
-        // given
-        BigDecimal leftOperand = new BigDecimal(leftOperandString);
-        BigDecimal rightOperand = new BigDecimal(rightOperandString);
-        BigDecimal expectedSum = new BigDecimal(expectedSumString);
-
-        // when
-        BigDecimal sum = StringCalculator.multiply(leftOperand, rightOperand);
-
-        // then
-        assertThat(sum).isEqualTo(expectedSum);
-    }
-
     @DisplayName(value = "두 수에 대한 나눗셈 테스트")
     @ParameterizedTest(name = "{index}. 피연산자: [{0},{1}] 결과: [{2}]")
     @CsvSource(value = {
