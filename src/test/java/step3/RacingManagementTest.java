96408b244b01c503c574fb63f7bbc1100ec9138a
김성규 <kskyu610@gmail.com>
2021-05-20 21:22:42 +0900

    test: 사용자로부터 입력 받은 수 만큼의 차량 생성 테스트 작성

---
+++ src/test/java/step3/RacingManagementTest.java
@@ -0,0 +1,24 @@
+package step3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingManagementTest {
+
+    @DisplayName(value = "사용자로부터 입력 받은 수 만큼의 차량 생성 테스트")
+    @Test
+    void createCarsTest() {
+        // given
+        int numberOfCars = 3;
+
+        // when
+        List<Car> cars = RacingManagement.createCars(numberOfCars);
+
+        // then
+        assertThat(cars.size()).isEqualTo(numberOfCars);
+    }
+}
