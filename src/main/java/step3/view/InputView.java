9accec43daff5476d6e438abaa24e56fb85bf2df
김성규 <kskyu610@gmail.com>
2021-05-20 21:17:20 +0900

    feat: InputView 클래스 - 자동차 대수와 시도할 횟수 입력 기능 구현

---
+++ src/main/java/step3/view/InputView.java
@@ -0,0 +1,21 @@
+package step3.view;
+
+import java.util.Scanner;
+
+public final class InputView {
+
+    private final static Scanner input = new Scanner(System.in);
+
+    private InputView() {
+    }
+
+    public final static int inputNumberOfCars() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return input.nextInt();
+    }
+
+    public final static int inputNumberOfPlay() {
+        System.out.println("시도할 횟수는 몇 회 인가요?");
+        return input.nextInt();
+    }
+}
