fee5c22e504cf0dd0df01504a4a46773c78af670
김성규 <kskyu610@gmail.com>
2021-05-21 00:25:27 +0900

    test: raceTest를 일반 Test로 변경 및 라운드 별 반복을 race 메소드 외부로 리팩토링

--- src/test/java/step3/RacingManagerTest.java
+++ src/test/java/step3/RacingManagerTest.java
@@ -3,14 +3,11 @@ package step3;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mockStatic;
@@ -37,20 +34,19 @@ public class RacingManagerTest {
         mockMovementConditionProvider.close();
     }
 
-    static Stream<Arguments> provideNumberOfRoundsForRaceTest() {
-        return Stream.of(
-                Arguments.of(3, new String[]{"----", "----", "----"})
-        );
-    }
+    @DisplayName(value = "라운드 수 만큼 경주를 진행하는 테스트")
+    @Test
+    void raceTest() {
 
-    @DisplayName(value = "사용자로부터 입력 받은 수 만큼의 라운드를 진행하는 경주 테스트")
-    @ParameterizedTest(name = "라운드 수: [{0}], 경주 종료 후 차량 위치: [{1}]")
-    @MethodSource(value = "provideNumberOfRoundsForRaceTest")
-    void raceTest(int numberOfRounds, String[] expectedCarPositions) {
+        // given
+        int numberOfRounds = 3;
+        String[] expectedCarPositions = new String[]{"----", "----", "----"};
 
         // when
         when(MovementConditionProvider.makeMovementCondition()).thenReturn(5);
-        racingManager.race(numberOfRounds, cars);
+        for (int round = 1; round <= numberOfRounds; round++) {
+            racingManager.race(cars);
+        }
         String[] carPositions = cars.stream()
                 .map(car -> car.getPosition())
                 .toArray(String[]::new);
