f2a8366a1aca021a75bb002b008728f0de7b32dc
김성규 <kskyu610@gmail.com>
2021-06-01 17:34:29 +0900

    test: MovementConditionProvider를 singleton으로 사용하도록 mock 리팩토링

--- src/test/java/step3/RacingManagerTest.java
+++ src/test/java/step3/RacingManagerTest.java
@@ -10,14 +10,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class RacingManagerTest {
 
     private static List<Car> cars;
     private static RacingManager racingManager;
-    private static MockedStatic<MovementConditionProvider> mockMovementConditionProvider;
+    private static MockedStatic<MovementConditionProvider> mockedStaticMovementConditionProvider;
+    private static MovementConditionProvider mockMovementConditionProvider;
 
     @BeforeAll
     static void setUp() {
@@ -25,25 +25,27 @@ public class RacingManagerTest {
         for (int i = 0; i < 3; i++) {
             cars.add(new Car());
         }
+        mockedStaticMovementConditionProvider = mockStatic(MovementConditionProvider.class);
+        mockMovementConditionProvider = mock(MovementConditionProvider.class);
+
+        when(MovementConditionProvider.getInstance()).thenReturn(mockMovementConditionProvider);
+        when(mockMovementConditionProvider.makeMovementCondition()).thenReturn(5);
         racingManager = RacingManager.getInstance();
-        mockMovementConditionProvider = mockStatic(MovementConditionProvider.class);
     }
 
     @AfterAll
     static void unset() {
-        mockMovementConditionProvider.close();
+        mockedStaticMovementConditionProvider.close();
     }
 
     @DisplayName(value = "라운드 수 만큼 경주를 진행하는 테스트")
     @Test
     void raceTest() {
-
         // given
         int numberOfRounds = 3;
         String[] expectedCarPositions = new String[]{"----", "----", "----"};
 
         // when
-        //when(MovementConditionProvider.makeMovementCondition()).thenReturn(5);
         for (int round = 1; round <= numberOfRounds; round++) {
             racingManager.race(cars);
         }
