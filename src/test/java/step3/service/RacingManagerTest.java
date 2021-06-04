3b6e2a3a4f38ea3215cf1ca79003029b2888902f
김성규 <kskyu610@gmail.com>
2021-06-04 17:39:50 +0900

    test: List<Cars>를 일급 컬랙션 Cars로 수정한 변경사항 적용

--- src/test/java/step3/service/RacingManagerTest.java
+++ src/test/java/step3/service/RacingManagerTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import step3.domain.Car;
-import step3.service.MovementConditionProvider;
-import step3.service.RacingManager;
+import step3.domain.Cars;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,17 +16,20 @@ import static org.mockito.Mockito.*;
 
 public class RacingManagerTest {
 
-    private static List<Car> cars;
+    private static Cars cars;
+    private static List<Car> wrappedCars;
     private static RacingManager racingManager;
     private static MockedStatic<MovementConditionProvider> mockedStaticMovementConditionProvider;
     private static MovementConditionProvider mockMovementConditionProvider;
 
     @BeforeAll
     static void setUp() {
-        cars = new ArrayList<>(3);
+        wrappedCars = new ArrayList<>(3);
         for (int i = 0; i < 3; i++) {
-            cars.add(new Car());
+            wrappedCars.add(new Car());
         }
+        cars = new Cars(wrappedCars);
+
         mockedStaticMovementConditionProvider = mockStatic(MovementConditionProvider.class);
         mockMovementConditionProvider = mock(MovementConditionProvider.class);
 
@@ -52,7 +54,7 @@ public class RacingManagerTest {
         for (int round = 1; round <= numberOfRounds; round++) {
             racingManager.race(cars);
         }
-        String[] carPositions = cars.stream()
+        String[] carPositions = wrappedCars.stream()
                 .map(car -> car.getPosition())
                 .toArray(String[]::new);
 
