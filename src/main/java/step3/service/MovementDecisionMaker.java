cb25d37e0bcb12c4b77992b80c9f727624767f98
김성규 <kskyu610@gmail.com>
2021-06-04 16:00:15 +0900

    refactor: MovementDecisionMaker 클래스 - singleton 패턴을 Eager Initialization에서 Initialization On Demand Holder Idiom으로 변경

--- src/main/java/step3/service/MovementDecisionMaker.java
+++ src/main/java/step3/service/MovementDecisionMaker.java
@@ -6,20 +6,21 @@ public final class MovementDecisionMaker {
     private final static boolean IS_MOVABLE = true;
     private final static boolean IS_NOT_MOVABLE = false;
 
-    private final static MovementDecisionMaker instance = new MovementDecisionMaker();
-
     private MovementDecisionMaker() {
     }
 
     public final static MovementDecisionMaker getInstance() {
-        return instance;
+        return MovementDecisionMakerHolder.instance;
     }
-
 
     public final boolean decideMovement(final int conditionToMove) {
         if (conditionToMove >= CRITERION_TO_MOVE) {
             return IS_MOVABLE;
         }
         return IS_NOT_MOVABLE;
+    }
+
+    private final static class MovementDecisionMakerHolder {
+        private final static MovementDecisionMaker instance = new MovementDecisionMaker();
     }
 }
