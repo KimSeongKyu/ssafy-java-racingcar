c565a56d0f20bcc25f1bfaf0739f483728b77fbd
김성규 <kskyu610@gmail.com>
2021-06-09 15:56:34 +0900

    refactor: InputView 클래스 - 매직 리터럴 상수화

--- src/main/java/step3/view/InputView.java
+++ src/main/java/step3/view/InputView.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 
 public final class InputView {
 
+    private final static String SENTENCE_FOR_INPUT_NUMBER_OF_CARS = "자동차 대수는 몇 대 인가요?";
+    private final static String SENTENCE_FOR_INPUT_NUMBER_OF_ROUNDS = "시도할 횟수는 몇 회 인가요?";
+
     private final static Scanner input = new Scanner(System.in);
 
     private InputView() {
     }
 
     public final static int inputNumberOfCars() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
+        System.out.println(SENTENCE_FOR_INPUT_NUMBER_OF_CARS);
         return input.nextInt();
     }
 
     public final static int inputNumberOfRounds() {
-        System.out.println("시도할 횟수는 몇 회 인가요?");
+        System.out.println(SENTENCE_FOR_INPUT_NUMBER_OF_ROUNDS);
         return input.nextInt();
     }
 }
