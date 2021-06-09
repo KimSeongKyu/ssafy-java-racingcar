b8fcb23d020332aff2e970002e743933f84bccf7
김성규 <kskyu610@gmail.com>
2021-06-09 17:52:43 +0900

    test: NumberOfCars 클래스 - 자동차 수와 동일한 값의 멤버 변수를 갖는지 확인 테스트 작성

--- src/test/java/step3/domain/car/NumberOfCarsTest.java
+++ src/test/java/step3/domain/car/NumberOfCarsTest.java
@@ -9,11 +9,22 @@ public class NumberOfCarsTest {
 
     @DisplayName("자동차의 수가 양수인지 검증하는 로직을 포함한 생성 테스트")
     @Test
-    void getNumberOfCars() {
+    void ConstructorTest() {
         // given and when
         NumberOfCars numberOfCars = new NumberOfCars(3);
 
         // then
         assertThat(numberOfCars).isNotNull();
+    }
+
+    @DisplayName("자동차 수와 동일한 값의 멤버 변수를 갖는지 확인 테스트")
+    @Test
+    void getNumberOfCarsTest() {
+        // given and when
+        int expectedNumberOfCars = 3;
+        NumberOfCars numberOfCars = new NumberOfCars(expectedNumberOfCars);
+
+        // then
+        assertThat(numberOfCars.getNumberOfCars()).isEqualTo(expectedNumberOfCars);
     }
 }
