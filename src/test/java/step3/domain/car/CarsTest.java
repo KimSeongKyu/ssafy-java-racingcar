0388b51c2a4ba1f6374ed714eb661f11db0d9a21
김성규 <kskyu610@gmail.com>
2021-06-11 15:36:07 +0900

    test: Cars 클래스 - 주어진 수만큼 Car 객체를 갖는지 확인하는 테스트 작성

--- src/test/java/step3/domain/car/CarsTest.java
+++ src/test/java/step3/domain/car/CarsTest.java
@@ -3,6 +3,8 @@ package step3.domain.car;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarsTest {
@@ -18,5 +20,19 @@ class CarsTest {
 
         // then
         assertThat(cars).isNotNull();
+    }
+
+    @DisplayName(value = "주어진 수만큼 Car 객체를 갖는지 확인하는 테스트")
+    @Test
+    void getCarsTest() {
+        // given
+        NumberOfCars numberOfCars = new NumberOfCars(3);
+        Cars cars = new Cars(numberOfCars);
+
+        // when
+        List<Car> carElements = cars.getCars();
+
+        // then
+        assertThat(carElements.size()).isEqualTo(3);
     }
 }
