116672da8120656d5ada1ec59275192622b362dd
김성규 <kskyu610@gmail.com>
2021-06-11 11:29:02 +0900

    test: MovementStrategySelector 클래스 - 이동 전략이 4일 경우 해당 이동 전략 인스턴스를 반환하는 테스트 작성

--- src/test/java/step3/domain/strategy/movement/MovementStrategySelectorTest.java
+++ src/test/java/step3/domain/strategy/movement/MovementStrategySelectorTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class MovementStrategySelectorTest {
 
-    @DisplayName(value = "이동 전략이 4일 경우 해당 전략 인스턴스를 반환하는 테스트")
+    @DisplayName(value = "이동 전략이 올바를 경우 인스턴스 반환 테스트")
     @Test
     void selectMovementStrategyTest() {
         // given
@@ -32,5 +32,19 @@ public class MovementStrategySelectorTest {
         assertThatExceptionOfType(IllegalMovementStrategyException.class).isThrownBy(() -> {
             MovementStrategySelector movementStrategySelector = MovementStrategySelector.selectMovementStrategy(movementStrategyNumber);
         });
+    }
+
+    @DisplayName(value = "이동 전략이 4일 경우 해당 이동 전략 인스턴스를 반환하는 테스트")
+    @Test
+    void getMovementStrategyTest() {
+        // given
+        int movementStrategyNumber = 4;
+        MovementStrategySelector movementStrategySelector = MovementStrategySelector.selectMovementStrategy(movementStrategyNumber);
+
+        // when
+        MovementStrategy movementStrategy = movementStrategySelector.getMovementStrategy();
+
+        // then
+        assertThat(movementStrategy).isInstanceOf(MovementStrategyWithFour.class);
     }
 }
