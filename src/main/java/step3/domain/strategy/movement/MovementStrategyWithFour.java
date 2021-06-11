6cb2dbef97ada0ec275ca06d4839db1587974752
김성규 <kskyu610@gmail.com>
2021-06-11 16:55:59 +0900

    refactor: MovementStrategyWithFour 클래스 - 코드 간결화

--- src/main/java/step3/domain/strategy/movement/MovementStrategyWithFour.java
+++ src/main/java/step3/domain/strategy/movement/MovementStrategyWithFour.java
@@ -3,8 +3,6 @@ package step3.domain.strategy.movement;
 public final class MovementStrategyWithFour implements MovementStrategy {
 
     private final static int CRITERION_TO_MOVE = 4;
-    private final static boolean IS_MOVABLE = true;
-    private final static boolean IS_NOT_MOVABLE = false;
 
     private MovementStrategyWithFour() {
     }
@@ -15,10 +13,7 @@ public final class MovementStrategyWithFour implements MovementStrategy {
 
     @Override
     public final boolean isMovable(final int movementStrategyNumber) {
-        if (movementStrategyNumber >= CRITERION_TO_MOVE) {
-            return IS_MOVABLE;
-        }
-        return IS_NOT_MOVABLE;
+        return movementStrategyNumber >= CRITERION_TO_MOVE;
     }
 
     private final static class MovementStrategyWithFourHolder {
