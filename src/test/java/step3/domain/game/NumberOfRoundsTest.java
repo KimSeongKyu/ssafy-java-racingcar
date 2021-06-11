7d0a2424e656bc36861484587ffe10bf2d3715bf
김성규 <kskyu610@gmail.com>
2021-06-11 15:44:05 +0900

    test: NumberOfRoundsOutOfBoundException 테스트를 NumberOfRounds 클래스의 생성 테스트 시나리오로 리팩토링

--- src/test/java/step3/domain/game/NumberOfRoundsTest.java
+++ src/test/java/step3/domain/game/NumberOfRoundsTest.java
@@ -2,19 +2,30 @@ package step3.domain.game;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step3.domain.exception.NumberOfRoundsOutOfBoundException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class NumberOfRoundsTest {
 
     @DisplayName("라운드 수가 양수인지 검증하는 로직을 포함한 생성 테스트")
     @Test
-    void ConstructorTest() {
+    void constructTest() {
         // given and when
         NumberOfRounds numberOfRounds = new NumberOfRounds(5);
 
         // then
         assertThat(numberOfRounds).isNotNull();
+    }
+
+    @DisplayName("라운드 수가 음수인 경우 예외를 발생시키는 테스트")
+    @Test
+    void constructWithNegativeNumberOfRoundsThrowExceptionTest() {
+        // given when then
+        assertThatExceptionOfType(NumberOfRoundsOutOfBoundException.class).isThrownBy(() -> {
+            new NumberOfRounds(-1);
+        });
     }
 
     @DisplayName("라운드 수와 동일한 값의 멤버 변수를 갖는지 확인 테스트")
