6e73388a6899089f461533908098389e5ff97dbb
김성규 <kskyu610@gmail.com>
2021-06-11 15:30:05 +0900

    test: Cars 클래스 - createCars 테스트를 생성자 테스트 시나리오 내부로 리팩토링

--- src/test/java/step3/domain/car/CarsTest.java
+++ src/test/java/step3/domain/car/CarsTest.java
@@ -3,26 +3,20 @@ package step3.domain.car;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarsTest {
 
-    @DisplayName(value = "사용자로부터 입력 받은 수 만큼의 차량 생성 테스트")
+    @DisplayName(value = "생성 테스트")
     @Test
-    void createCarsTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    void ConstructorTest() {
         // given
-        int numberOfCars = 3;
-        Method method = Cars.class.getDeclaredMethod("createCars", int.class);
-        method.setAccessible(true);
+        NumberOfCars numberOfCars = new NumberOfCars(3);
 
         // when
-        List<Car> cars = (List<Car>) method.invoke(null, numberOfCars);
+        Cars cars = new Cars(numberOfCars);
 
         // then
-        assertThat(cars.size()).isEqualTo(numberOfCars);
+        assertThat(cars).isNotNull();
     }
 }
