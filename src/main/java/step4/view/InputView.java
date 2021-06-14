3f10b0b274426ee4da4ee1082915e3f346b4580c
김성규 <kskyu610@gmail.com>
2021-06-14 15:33:30 +0900

    feat: InputView 클래스 - 이름 입력 기능 구현

---
+++ src/main/java/step4/view/InputView.java
@@ -0,0 +1,12 @@
+package step4.view;
+
+import java.util.Scanner;
+
+public final class InputView {
+
+    private final static Scanner INPUT = new Scanner(System.in);
+
+    public final static String inputNames() {
+        return INPUT.next();
+    }
+}
