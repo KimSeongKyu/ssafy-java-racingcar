52b87c3cef56fa5dba725db65f51578b86a098fd
김성규 <kskyu610@gmail.com>
2021-06-14 16:23:24 +0900

    feat: 문자열을 ','로 분리 시 null 혹은 빈 문자열일 경우 예외를 발생하는 기능 구현

--- src/main/java/step4/exception/SentenceEmptyException.java
+++ src/main/java/step4/exception/SentenceEmptyException.java
@@ -1,4 +1,11 @@
 package step4.exception;
 
-public class SentenceEmptyException extends RuntimeException {
+public final class SentenceEmptyException extends RuntimeException {
+
+    private final static String EMPTY_SENTENCE_MESSAGE = "빈 문자열입니다.";
+
+    @Override
+    public String toString() {
+        return EMPTY_SENTENCE_MESSAGE;
+    }
 }
--- src/main/java/step4/util/StringUtility.java
+++ src/main/java/step4/util/StringUtility.java
@@ -1,10 +1,29 @@
 package step4.util;
 
+import step4.exception.SentenceEmptyException;
+import step4.exception.SentenceNullPointerException;
+
+import java.util.Objects;
+
 public final class StringUtility {
 
     private final static String COMMA = ",";
 
     public final static String[] splitByComma(final String sentence) {
+        validateSentenceIsNull(sentence);
+        validateSentenceIsEmpty(sentence);
         return sentence.split(COMMA);
+    }
+
+    private final static void validateSentenceIsNull(final String sentence) {
+        if (Objects.isNull(sentence)) {
+            throw new SentenceNullPointerException();
+        }
+    }
+
+    private final static void validateSentenceIsEmpty(final String sentence) {
+        if (sentence.isEmpty()) {
+            throw new SentenceEmptyException();
+        }
     }
 }
