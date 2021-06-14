92134c2ad9940969337f6d5211995dbc4860bf73
김성규 <kskyu610@gmail.com>
2021-06-14 17:55:42 +0900

    feat: Names 클래스 - 이름 리스트를 갖는 생성 기능 구현

--- src/main/java/step4/domain/name/Names.java
+++ src/main/java/step4/domain/name/Names.java
@@ -1,4 +1,19 @@
 package step4.domain.name;
 
-public class Names {
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Names {
+
+    private final List<Name> names;
+
+    public Names(final List<String> namesAsString) {
+        this.names = createNames(namesAsString);
+    }
+
+    private final List<Name> createNames(final List<String> namesAsString) {
+        List<Name> names = new ArrayList<>();
+        namesAsString.stream().forEachOrdered(name -> names.add(new Name(name)));
+        return names;
+    }
 }
