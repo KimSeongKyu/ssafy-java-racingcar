7ca666077a5e82147f63ec640023b395f0f720a7
김성규 <kskyu610@gmail.com>
2021-06-14 18:20:45 +0900

    test: Names 클래스 - null 혹은 빈 리스트를 파라미터로 생성 시 예외가 발생하는 테스트 작성

--- src/test/java/step4/domain/name/NamesTest.java
+++ src/test/java/step4/domain/name/NamesTest.java
@@ -2,14 +2,27 @@ package step4.domain.name;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class NamesTest {
+
+    static Stream<Arguments> provideNullAndEmptyListForConstructThrowExceptionTest() {
+        return Stream.of(
+                Arguments.of(null, NamesNullPointerException.class),
+                Arguments.of(new ArrayList<String>(), NamesEmptyException.class)
+        );
+    }
 
     @DisplayName(value = "이름 리스트를 갖는 생성 테스트")
     @Test
@@ -22,5 +35,14 @@ class NamesTest {
 
         // then
         assertThat(names).isNotNull();
+    }
+
+
+    @DisplayName(value = "null 혹은 빈 리스트를 파라미터로 생성 시 예외가 발생하는 테스트")
+    @ParameterizedTest(name = "{index}. 리스트: {0} 발생하는 예외: {1}")
+    @MethodSource(value = "provideNullAndEmptyListForConstructThrowExceptionTest")
+    void constructThrowExceptionTest(List<String> names, Class exception) {
+        // when and then
+        assertThatExceptionOfType(exception).isThrownBy(() -> new Names(names));
     }
 }
