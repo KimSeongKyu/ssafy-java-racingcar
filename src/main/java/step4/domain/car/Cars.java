c4c448f2959304a27db089b9b5e87a3e0d7cdad8
김성규 <kskyu610@gmail.com>
2021-06-15 16:16:53 +0900

    feat: Cars 클래스 - 자동차 리스트를 갖는 생성 기능 구현

--- src/main/java/step4/domain/car/Cars.java
+++ src/main/java/step4/domain/car/Cars.java
@@ -1,4 +1,26 @@
 package step4.domain.car;
 
-public class Cars {
+import step4.domain.name.Name;
+import step4.domain.name.Names;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Cars {
+
+    private final List<Car> cars;
+
+    public Cars(final Names names) {
+        this(names.names());
+    }
+
+    public Cars(final List<Name> names) {
+        this.cars = createCars(names);
+    }
+
+    private final List<Car> createCars(final List<Name> names) {
+        List<Car> cars = new ArrayList<>();
+        names.stream().forEachOrdered(name -> cars.add(new Car(name)));
+        return cars;
+    }
 }
