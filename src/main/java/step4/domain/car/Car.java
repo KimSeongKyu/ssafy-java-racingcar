7ebf4419886f30aa2460055a2bfb6604a732fbf3
김성규 <kskyu610@gmail.com>
2021-06-15 15:34:07 +0900

    feat: Car 클래스 - 이름을 갖는 자동차 생성 기능 구현

--- src/main/java/step4/domain/car/Car.java
+++ src/main/java/step4/domain/car/Car.java
@@ -1,4 +1,16 @@
 package step4.domain.car;
 
-public class Car {
+import step4.domain.name.Name;
+
+public final class Car {
+
+    private final static int START_POSITION = 1;
+
+    private final Name name;
+    private final int position;
+
+    public Car(final Name name) {
+        this.name = name;
+        this.position = START_POSITION;
+    }
 }
