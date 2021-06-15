8759be49f128cfa5c791a71ff205c9c7325be24e
김성규 <kskyu610@gmail.com>
2021-06-15 18:08:24 +0900

    feat: NumberOfRounds 클래스 - 라운드 수를 갖는 생성 기능 구현

--- src/main/java/step4/domain/round/NumberOfRounds.java
+++ src/main/java/step4/domain/round/NumberOfRounds.java
@@ -1,4 +1,10 @@
 package step4.domain.round;
 
-public class NumberOfRounds {
+public final class NumberOfRounds {
+
+    private final int numberOfRounds;
+
+    public NumberOfRounds(final int numberOfRounds) {
+        this.numberOfRounds = numberOfRounds;
+    }
 }
