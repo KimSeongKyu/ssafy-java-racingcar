a26a39c3f81d255e09605d96dbd907dcf5cc1cbb
김성규 <kskyu610@gmail.com>
2021-06-15 18:11:21 +0900

    test: NumberOfRounds 클래스 - 라운드 수가 양수가 아닐 경우 생성 시 예외가 발생하는 테스트 작성

--- src/test/java/step4/domain/round/NumberOfRoundsTest.java
+++ src/test/java/step4/domain/round/NumberOfRoundsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class NumberOfRoundsTest {
 
@@ -18,5 +19,17 @@ class NumberOfRoundsTest {
 
         // then
         assertThat(numberOfRounds).isNotNull();
+    }
+
+    @DisplayName(value = "라운드 수가 양수가 아닐 경우 생성 시 예외가 발생하는 테스트")
+    @Test
+    void constructWithNotPositiveNumberThrowExceptionTest() {
+        // given
+        int numberOfRoundsValue = -1;
+
+        // when and then
+        assertThatExceptionOfType(NumberOfRoundsNotPositiveException.class).isThrownBy(() -> {
+            new NumberOfRounds(numberOfRoundsValue);
+        });
     }
 }
