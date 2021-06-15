08513972f004600bfaa8d450268915809f63634d
김성규 <kskyu610@gmail.com>
2021-06-15 18:00:01 +0900

    feat: Application 클래스 - 이름을 갖는 자동차 생성 기능 구현

--- src/main/java/step4/Application.java
+++ src/main/java/step4/Application.java
@@ -1,5 +1,6 @@
 package step4;
 
+import step4.domain.car.Cars;
 import step4.domain.name.Names;
 import step4.util.StringUtility;
 import step4.view.InputView;
@@ -8,5 +9,6 @@ public class Application {
 
     public static void main(String[] args) {
         Names names = new Names(StringUtility.splitByComma(InputView.inputNames()));
+        Cars cars = new Cars(names);
     }
 }
