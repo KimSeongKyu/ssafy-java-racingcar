fd7b2a1598e132106da69cfd865a64ac85477403
김성규 <kskyu610@gmail.com>
2021-06-15 18:04:12 +0900

    feat: InputView 클래스 - 라운드 수 입력 기능 구현

--- src/main/java/step4/view/InputView.java
+++ src/main/java/step4/view/InputView.java
@@ -4,9 +4,15 @@ import java.util.Scanner;
 
 public final class InputView {
 
+    private final static String INPUT_NUMBER_OF_ROUNDS_MESSAGE = "시도할 회수는 몇회인가요?";
     private final static Scanner INPUT = new Scanner(System.in);
 
     public final static String inputNames() {
         return INPUT.next();
+    }
+
+    public final static int inputNumberOfRounds() {
+        System.out.println(INPUT_NUMBER_OF_ROUNDS_MESSAGE);
+        return INPUT.nextInt();
     }
 }
