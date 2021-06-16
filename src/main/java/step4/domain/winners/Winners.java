ec02d69bffd47dfe170a9496dd54487b9b2af91e
김성규 <kskyu610@gmail.com>
2021-06-16 11:18:27 +0900

    feat: Winners 클래스 - 생성 기능 구현

--- src/main/java/step4/domain/winners/Winners.java
+++ src/main/java/step4/domain/winners/Winners.java
@@ -1,4 +1,12 @@
 package step4.domain.winners;
 
-public class Winners {
+import step4.domain.car.Cars;
+
+public final class Winners {
+
+    private final Cars cars;
+
+    public Winners(final Cars cars) {
+        this.cars = cars;
+    }
 }
