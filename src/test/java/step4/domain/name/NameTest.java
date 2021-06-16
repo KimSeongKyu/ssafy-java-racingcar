e82ad32a9f96953fa5b5203849fc4f48f903d00e
김성규 <kskyu610@gmail.com>
2021-06-16 15:45:18 +0900

    test: Name 클래스 - 생성 시 발생할 수 있는 예외 상황 추가 리팩토링

--- src/test/java/step4/domain/name/NameTest.java
+++ src/test/java/step4/domain/name/NameTest.java
@@ -2,12 +2,26 @@ package step4.domain.name;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import step4.exception.name.NameLengthOutOfBoundException;
+import step4.exception.name.NameNullPointerException;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class NameTest {
+
+    static Stream<Arguments> provideNameAsStringForConstructThrowExceptionTest() {
+        return Stream.of(
+                Arguments.of(null, new NameNullPointerException()),
+                Arguments.of("", new EmptyNameException()),
+                Arguments.of("KimSeongKyu", new NameLengthOutOfBoundException())
+        );
+    }
 
     @DisplayName(value = "문자열 이름을 멤버 변수로 갖는 객체 생성 테스트")
     @Test
@@ -22,15 +36,13 @@ class NameTest {
         assertThat(name).isNotNull();
     }
 
-    @DisplayName(value = "문자열의 길이가 5 초과인 경우 생성 시 예외가 발생하는 테스트")
-    @Test
-    void constructThrowExceptionTest() {
-        // given
-        String nameWithLengthLongerThanFive = "kimSeongKyu";
-
+    @DisplayName(value = "String형 이름이 null, 빈 문자열, 혹은 5글자 초과인 경우 생성 시 예외가 발생하는 테스트")
+    @ParameterizedTest(name = "{index}. 이름: {0} 발생하는 예외: {1}")
+    @MethodSource(value = "provideNameAsStringForConstructThrowExceptionTest")
+    void constructThrowExceptionTest(String name, RuntimeException exception) {
         // when and then
-        assertThatExceptionOfType(NameLengthOutOfBoundException.class).isThrownBy(() -> {
-            new Name(nameWithLengthLongerThanFive);
+        assertThatExceptionOfType(exception.getClass()).isThrownBy(() -> {
+            new Name(name);
         });
     }
 
