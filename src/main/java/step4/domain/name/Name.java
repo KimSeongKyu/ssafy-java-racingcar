f1987b57fe9c4104d45d85fe44c540ce4ea26b7d
김성규 <kskyu610@gmail.com>
2021-06-16 15:54:41 +0900

    refactor: Name 클래스 - 발생 가능한 예외 상황 추가 리팩토링

--- src/main/java/step4/domain/name/Name.java
+++ src/main/java/step4/domain/name/Name.java
@@ -1,22 +1,31 @@
 package step4.domain.name;
 
 import step4.exception.name.NameLengthOutOfBoundException;
+import step4.exception.name.NameNullPointerException;
 
 import java.util.Objects;
 
 public final class Name {
 
+    private final static int EMPTY = 0;
     private final static int BOUND = 5;
 
     private final String value;
 
     public Name(final String name) {
+        validateNameIsNull(name);
         validateNameLengthInBound(name);
         value = name;
     }
 
+    private final void validateNameIsNull(final String name) {
+        if (Objects.isNull(name)) {
+            throw new NameNullPointerException();
+        }
+    }
+
     private final void validateNameLengthInBound(final String name) {
-        if (name.length() > BOUND) {
+        if (!(EMPTY < name.length() && name.length() <= BOUND)) {
             throw new NameLengthOutOfBoundException();
         }
     }
--- src/main/java/step4/exception/name/NameLengthOutOfBoundException.java
+++ src/main/java/step4/exception/name/NameLengthOutOfBoundException.java
@@ -2,7 +2,7 @@ package step4.exception.name;
 
 public class NameLengthOutOfBoundException extends RuntimeException {
 
-    private final static String NAME_LENGTH_OUT_OF_BOUND_MESSAGE = "이름은 5글자 이하여야 합니다.";
+    private final static String NAME_LENGTH_OUT_OF_BOUND_MESSAGE = "이름은 1~5글자 이내여야 합니다.";
 
     @Override
     public String toString() {
