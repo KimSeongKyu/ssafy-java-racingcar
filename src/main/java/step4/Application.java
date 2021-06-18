8b440f9d6aec65e65bbd1eb7509f2f50f5ea670d
김성규 <kskyu610@gmail.com>
2021-06-18 17:47:53 +0900

    refactor: Application 클래스 - 재귀를 통한 무중단 프로그래밍으로 리팩토링

--- src/main/java/step4/Application.java
+++ src/main/java/step4/Application.java
@@ -15,13 +15,13 @@ public class Application {
     private final static int CRITERION_FOR_MOVE = 4;
 
     public static void main(String[] args) {
-        final Names names = new Names(StringUtility.splitByComma(InputView.inputNames()));
-        Cars cars = new Cars(names);
+        final Names names = inputNames();
+        Cars cars = createCars(names);
 
-        Round round = new Round(InputView.inputFinalRound());
+        Round round = inputFinalRound();
 
         OutputView.printResultSentence();
-        while(round.hasNext()) {
+        while (round.hasNext()) {
             cars = cars.move(() -> MovementCondition.generateRandomNumberInBound(MOVEMENT_CONDITION_BOUND) >= CRITERION_FOR_MOVE);
 
             OutputView.printRacingResultPerRound(cars);
@@ -29,5 +29,32 @@ public class Application {
             round = round.next();
         }
         OutputView.printWinnersNames(new Winners(cars));
+    }
+
+    private final static Names inputNames() {
+        try {
+            return new Names(StringUtility.splitByComma(InputView.inputNames()));
+        } catch (Exception exception) {
+            System.out.println(exception);
+            return inputNames();
+        }
+    }
+
+    private final static Cars createCars(Names names) {
+        try {
+            return new Cars(names);
+        } catch (Exception exception) {
+            System.out.println(exception);
+            return createCars(inputNames());
+        }
+    }
+
+    private final static Round inputFinalRound() {
+        try {
+            return new Round(InputView.inputFinalRound());
+        } catch (Exception exception) {
+            System.out.println(exception);
+            return inputFinalRound();
+        }
     }
 }
