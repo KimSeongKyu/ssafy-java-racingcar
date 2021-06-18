10d3261a2e6fc067df7057f44abcd8e066d23897
김성규 <kskyu610@gmail.com>
2021-06-18 14:50:14 +0900

    refactor: Application 클래스 - 반복문의 제어식 내 메소드 중복 호출 제거 및 직관적 단어로 리팩토링

--- src/main/java/step4/Application.java
+++ src/main/java/step4/Application.java
@@ -11,7 +11,7 @@ import step4.view.OutputView;
 
 public class Application {
 
-    private final static int START_ROUND = 1;
+    private final static int FIRST_ROUND = 1;
     private final static int MOVEMENT_CONDITION_BOUND = 10;
     private final static int CRITERION_FOR_MOVE = 4;
 
@@ -22,7 +22,8 @@ public class Application {
         final TotalRound totalRound = new TotalRound(InputView.inputTotalRound());
 
         OutputView.printResultSentence();
-        for (int round = START_ROUND; round <= totalRound.value(); round++) {
+        final int lastRound = totalRound.value();
+        for (int round = FIRST_ROUND; round <= lastRound; round++) {
             cars = cars.move(() -> MovementCondition.generateRandomNumberInBound(MOVEMENT_CONDITION_BOUND) >= CRITERION_FOR_MOVE);
             OutputView.printCarPositions(cars);
         }
