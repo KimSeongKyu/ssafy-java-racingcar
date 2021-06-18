69dc38ec86e0f9946f0e6bd9c242c5d53409c77d
김성규 <kskyu610@gmail.com>
2021-06-18 16:29:19 +0900

    feat: Round 클래스 - 다음 라운드로 진행하는 기능 구현

--- src/main/java/step4/domain/round/Round.java
+++ src/main/java/step4/domain/round/Round.java
@@ -2,16 +2,25 @@ package step4.domain.round;
 
 import step4.exception.round.FinalRoundNotPositiveException;
 
+import java.util.Objects;
+
 public final class Round {
 
     public final static int FIRST_ROUND = 1;
 
     private final static int CRITERION_FOR_POSITIVE_NUMBER = 1;
+    private final static int NEXT = 1;
 
+    private final int currentRound;
     private final int finalRound;
 
     public Round(final int finalRound) {
+        this(FIRST_ROUND, finalRound);
+    }
+
+    public Round(final int currentRound, final int finalRound) {
         validateFinalRoundIsPositive(finalRound);
+        this.currentRound = currentRound;
         this.finalRound = finalRound;
     }
 
@@ -19,5 +28,22 @@ public final class Round {
         if (finalRound < CRITERION_FOR_POSITIVE_NUMBER) {
             throw new FinalRoundNotPositiveException();
         }
+    }
+
+    public final Round next() {
+        return new Round(currentRound + NEXT, finalRound);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Round round = (Round) o;
+        return currentRound == round.currentRound && finalRound == round.finalRound;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currentRound, finalRound);
     }
 }
