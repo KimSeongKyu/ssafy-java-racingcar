e818c58f2d23fe81aad1cb3e55477d55d8228fec
김성규 <kskyu610@gmail.com>
2021-06-18 16:35:56 +0900

    test: Round 클래스 - 생성 시 현재 라운드가 1 미만일 경우에도 예외가 발생하도록 예외 상황 추가 리팩토링

--- src/test/java/step4/domain/round/RoundTest.java
+++ src/test/java/step4/domain/round/RoundTest.java
@@ -2,12 +2,24 @@ package step4.domain.round;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import step4.exception.round.FinalRoundNotPositiveException;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class RoundTest {
+
+    static Stream<Arguments> provideCurrentAndFinalRoundsForConstructThrowExceptionTest() {
+        return Stream.of(
+                Arguments.of(0, 1, new CurrentRoundNotPositiveException()),
+                Arguments.of(1, 0, new FinalRoundNotPositiveException())
+        );
+    }
 
     @DisplayName(value = "생성 테스트")
     @Test
@@ -22,15 +34,13 @@ class RoundTest {
         assertThat(resultRound).isNotNull();
     }
 
-    @DisplayName(value = "마지막 라운드가 1 미만일 경우 생성 시 예외가 발생하는 테스트")
-    @Test
-    void constructThrowExceptionTest() {
-        // given
-        int finalRound = -1;
-
+    @DisplayName(value = "현재 혹은 최종 라운드가 1 미만일 경우 생성 시 예외가 발생하는 테스트")
+    @ParameterizedTest(name = "{index}. 현재 라운드: {0} 최종 라운드: {1} 발생하는 예외: {2}")
+    @MethodSource(value = "provideCurrentAndFinalRoundsForConstructThrowExceptionTest")
+    void constructThrowExceptionTest(int currentRound, int finalRound, RuntimeException exception) {
         // when and then
-        assertThatExceptionOfType(FinalRoundNotPositiveException.class).isThrownBy(() -> {
-            new Round(finalRound);
+        assertThatExceptionOfType(exception.getClass()).isThrownBy(() -> {
+            new Round(currentRound, finalRound);
         });
     }
 
